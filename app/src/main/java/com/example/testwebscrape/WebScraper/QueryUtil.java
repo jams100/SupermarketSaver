e6dd47150cc36f482caa1204e4bd1e4205cb62fc
jams100 <43343134+jams100@users.noreply.github.com>
2020-03-11 01:21:45 +0000

    Fixed linking products to their appropriate website link

--- app/src/main/java/com/example/testwebscrape/WebScraper/QueryUtil.java
+++ app/src/main/java/com/example/testwebscrape/WebScraper/QueryUtil.java
@@ -8,17 +8,8 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class QueryUtil {
@@ -29,7 +20,6 @@ public class QueryUtil {
     private static String superUrl;
 
     public QueryUtil() {
-
     }
 
     /**
@@ -55,69 +45,77 @@ public class QueryUtil {
             doc = Jsoup.connect(url).get();
             superVdoc = Jsoup.connect(superUrl).get();
 
-        //TESCO Webscraping
-        for (Element row : doc.select("div.desc")) {
-            Products pro;
+            //TESCO Webscraping
+            for (Element row : doc.select("div.desc")) {
+                Products pro;
 
-            if (!row.select("a.id").text().equals("")) {
-                continue;
-            } else {
-                String imageurl = row.select("img[src]").attr("src");
-                String productLink = row.select("a[href]").attr("href");
+                if (!row.select("a.id").text().equals("")) {
+                    continue;
+                } else {
+                    String imageurl = row.select("img[src]").attr("src");
+                    String productLink = "https://www.tesco.ie";
+                    String s = row.select("a[href]").attr("href");
+                    s = s.replace(" ", "+");
+                    productLink += s;
+                    String productdescription = row.select("h3.inBasketInfoContainer").text();
+                    String NewPrice = "";   //row.select("span.linePrice").text();
+                    String oldPrice = "";
+                    String imglogo = "https://www.retail-fmcg.ro/wp-content/uploads/2010/11/Copy-of-tesco_logo.jpeg";
 
-//                String productLink = "https://www.tesco.ie";
-//                String s = row.select("a[ref]").attr("href");
-//                s = s.replace(" ", "+");
-//                productLink += s;
-
-                //String productLink = "https://www.tesco.ie/groceries/Product/Details/?id=259009761";
-
-                String productdescription = row.select("h3.inBasketInfoContainer").text();
-                String NewPrice = "";   //row.select("span.linePrice").text();
-                String oldPrice = "";
-                String imglogo = "https://www.retail-fmcg.ro/wp-content/uploads/2010/11/Copy-of-tesco_logo.jpeg";
-
-                pro = new Products(productdescription, oldPrice, imageurl, productLink, imglogo, NewPrice);
+                    pro = new Products(productdescription, oldPrice, imageurl, productLink, imglogo, NewPrice);
+                }
+                products.add(pro);
             }
-            products.add(pro);
+            //To Get Price for Tesco Products
+            int count = 0;
+            for (Element row : doc.select("div.quantity")) {
+                Products pro = null;
 
-        }
-        //To Get Price for Tesco Products
-        int count = 0;
-        for (Element row : doc.select("div.quantity")) {
-            Products pro = null;
+                if (!row.select("a.id").text().equals("")) {
+                    continue;
+                } else {
+                    String NewPrice = row.select("span.linePrice").text();
+                    //String priceOld=row.select("em").text();
+                    products.get(count).setNewPrice(NewPrice);
+                    //products.get(count).setpriceOld(priceOld);
 
-            if (!row.select("a.id").text().equals("")) {
-                continue;
-            } else {
-                String NewPrice = row.select("span.linePrice").text();
-                //String priceOld=row.select("em").text();
-                products.get(count).setNewPrice(NewPrice);
-                //products.get(count).setpriceOld(priceOld);
-
-                count++;
+                    count++;
+                }
             }
-        }
 
-        //SUPERVALU Webscraping
-        for (Element row : superVdoc.select("div.product-list-item-details")) {
-            Products pro1;
+            //SUPERVALU Webscraping
+            for (Element row : superVdoc.select("div.product-list-item-details")) {
+                Products pro1;
 
-            if (row.select("h4.product-list-item-details-title").text().equals("")) {
-                continue;
-            } else {
-                //String imageurl = "https://d2wwnnx8tks4e8.cloudfront.net/images/app/medium/5011001295277_2.JPG";
-                String productLink = row.select("a[href]").attr("href");
-                String productdescription = row.select("h4.product-list-item-details-title").text();
-                String NewPrice = row.select("span[data-unit-price]").attr("data-unit-price"); //row.select("span.linePrice").text();
-                String oldPrice = "";
-                String imglogo = "https://www.independent.ie/business/personal-finance/article31444718.ece/5fab8/AUTOCROP/w620/2015-08-13_bus_11776288_I4.JPG";
-                String Imageurl = null;
+                if (row.select("h4.product-list-item-details-title").text().equals("")) {
+                    continue;
+                } else {
+                    //String imageurl = "https://d2wwnnx8tks4e8.cloudfront.net/images/app/medium/5011001295277_2.JPG";
+                    String productLink = row.select("a[href]").attr("href");
+                    String productdescription = row.select("h4.product-list-item-details-title").text();
+                    String NewPrice = row.select("span[data-unit-price]").attr("data-unit-price"); //row.select("span.linePrice").text();
+                    String oldPrice = "";
+                    String imglogo = "https://www.independent.ie/business/personal-finance/article31444718.ece/5fab8/AUTOCROP/w620/2015-08-13_bus_11776288_I4.JPG";
+                    String Imageurl = null;
 
-                pro1 = new Products(productdescription, oldPrice, Imageurl, productLink, imglogo, NewPrice);
+                    pro1 = new Products(productdescription, oldPrice, Imageurl, productLink, imglogo, NewPrice);
+                }
+                products.add(pro1);
             }
-            products.add(pro1);
-        }
+
+              //To Get Image For Supervalu
+//            int county = 0;
+//            for (Element row : superVdoc.select("div.product-list-item-display")) {
+//                if (!row.select("img.src").text().equals("")) {
+//                    continue;
+//                } else if (products.get(county).getImageLogo().equals("https://www.independent.ie/business/personal-finance/article31444718.ece/5fab8/AUTOCROP/w620/2015-08-13_bus_11776288_I4.JPG")) {
+//                    String Imageurl = row.select("img[data-src]").attr("data-src");
+//                    products.get(county).setNewImage(Imageurl);
+//                    county++;
+//                } else {
+//                    county++;
+//                }
+//            }
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -126,21 +124,6 @@ public class QueryUtil {
             // with the message from the exception.
             Log.e("QueryUtil", "Problem parsing results", e);
         }
-
-
-//        //To Get Image For Supervalu
-//        int county = 0;
-//        for (Element row : superVdoc.select("div.product-list-item-display")) {
-//            Products pro1 = null;
-//
-//            if (!row.select("img.src").text().equals("")) {
-//                continue;
-//            } else {
-//                String Imageurl = row.select("img[data-src]").attr("data-src");
-//                products.get(county).setNewImage(Imageurl);
-//                county++;
-//            }
-//        }
 
         // Return the list of products
         return products;

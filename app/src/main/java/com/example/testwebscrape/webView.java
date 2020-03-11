96ce7046ced93b0e4e7a5d20691efa82e0085f3a
jams100 <43343134+jams100@users.noreply.github.com>
2020-03-11 03:51:40 +0000

    Firebase update. Also removed actionbar on homepage as well as other minor updates

--- app/src/main/java/com/example/testwebscrape/webView.java
+++ app/src/main/java/com/example/testwebscrape/webView.java
@@ -1,24 +1,30 @@
 package com.example.testwebscrape;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class webView extends AppCompatActivity {
+    private FirebaseAnalytics myFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
 
+        //Obtaining the FirebaseAnalytics instance.
+        myFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
         WebView web=findViewById(R.id.web);
 
         web.setWebViewClient(new WebViewClient());
-//        WebSettings webSettings=web.getSettings();
-//        webSettings.setJavaScriptEnabled(true);
-
+        WebSettings webSettings=web.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
         Bundle bundle=getIntent().getExtras();
         web.loadUrl(bundle.getString("UrlWebLink"));
